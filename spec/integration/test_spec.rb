08abcfa4c8ba53e6cfe5798384d9d103fea199f4
Leland Garofalo <lgarofalo@users.noreply.github.com>
2019-01-13 19:44:53 -0800

    chore(rubocop): rab rubocop --auto-correct

--- spec/integration/test_spec.rb
+++ spec/integration/test_spec.rb
@@ -12,16 +12,16 @@ RSpec.describe 'Sheltertech API', type: :request, swagger_doc: 'v1/swagger.json'
       response '200', 'categories found' do
         schema type: :object,
                properties: {
-                   categories: {
-                       type: :array,
-                       properties: {
-                           id: { type: :integer },
-                           name: { type: :string },
-                           top_level: { type: :string },
-                           featured: { type: :boolean }
-                       },
-                       required: %w[id name top_level featured]
-                   }
+                 categories: {
+                   type: :array,
+                   properties: {
+                     id: { type: :integer },
+                     name: { type: :string },
+                     top_level: { type: :string },
+                     featured: { type: :boolean }
+                   },
+                   required: %w[id name top_level featured]
+                 }
                },
                required: %w[categories]
         let(:id) { Category.create(name: 'foo', vocabulary: 'bar').id }
